cba9fc361a5a586cec45e531d3b267595e2d0c3d
kougei-abe <g1727005@st.t-kougei.ac.jp>
2018-07-23 18:02:35 +0900

    レベリング

--- AppleCatch/Assets/GameDirector.cs
+++ AppleCatch/Assets/GameDirector.cs
@@ -7,8 +7,9 @@ public class GameDirector : MonoBehaviour {
 
     GameObject timerText;
     GameObject pointText;
-    float time = 60.0f;
+    float time = 30.0f;
     int point = 0;
+    GameObject generator;
 
     public void GetApple()
     {
@@ -22,6 +23,7 @@ public class GameDirector : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        this.generator = GameObject.Find("ItemGenerator");
         this.timerText = GameObject.Find("Time");
         this.pointText = GameObject.Find("Point");
 	}
@@ -29,6 +31,29 @@ public class GameDirector : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.time -= Time.deltaTime;
+
+        if(this.time < 0)
+        {
+            this.time = 0;
+            this.generator.GetComponent<ItemGeneraor>().SetParameter(10000.0f, 0, 0);
+        }
+        else if (0 <= this.time && this.time < 5)
+        {
+            this.generator.GetComponent<ItemGeneraor>().SetParameter(0.6f, -0.04f, 3);
+        }
+        else if (5 <= this.time && this.time < 15)
+        {
+            this.generator.GetComponent<ItemGeneraor>().SetParameter(0.4f, -0.05f, 5);
+        }
+        else if (15 <= this.time && this.time < 25)
+        {
+            this.generator.GetComponent<ItemGeneraor>().SetParameter(0.7f, -0.04f, 3);
+        }
+        else if (25 <= this.time && this.time < 30)
+        {
+            this.generator.GetComponent<ItemGeneraor>().SetParameter(0.9f, -0.03f, 2);
+        }
+
         this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
         this.pointText.GetComponent<Text>().text = this.point.ToString() + " point";
 	}
